643e1fc1a0507ec419c350c6b1cba256909deeb2
frank <zyming.x@foxmail.com>
2014-06-20 11:39:27 +0800

    test

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	    "mysql" => array(                 //数据库链接设置
 			'host' 		=> 'localhost',  // 服务器
 			'user' 		=> 'root',  	// 用户名
-			'password'	=> '123456', 		   // 密码
+			'password'	=> '', 		   // 密码
 			'database'	=> 'yocms',	  // 数据库
 		)
 	);
